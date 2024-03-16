0b0c0bdcf91447d06af53e52bd83173d42e828f7
Howard Yun <webirang@gmail.com>
2024-03-15 22:34:40 -0400

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import TextFileld from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { withStyles } from '@mui/styles';
+
+const styles = theme => ({
+  hidden: {
+    display: 'none'
+  }
+})
 
 class CustomerAdd extends React.Component {
 
@@ -11,7 +24,8 @@ class CustomerAdd extends React.Component {
       birthday: '',
       gender: '',
       job: '',
-      fileName: ''
+      fileName: '',
+      open: false
     }
   }
 
@@ -29,7 +43,8 @@ class CustomerAdd extends React.Component {
       birthday: '',
       gender: '',
       job: '',
-      fileName: ''
+      fileName: '',
+      open: false
     })
     // window.location.reload();
     // this.props.stateRefresh();
@@ -46,6 +61,24 @@ class CustomerAdd extends React.Component {
     let nextState = {};
     nextState[e.target.name] = e.target.value;
     this.setState(nextState);
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      file: null,
+      userName: '',
+      birthday: '',
+      gender: '',
+      job: '',
+      fileName: '',
+      open: false
+    })
   }
 
   addCustomer = () => {
@@ -66,21 +99,46 @@ class CustomerAdd extends React.Component {
     return axios.post(url, formData, config);
   }
 
-  // AxiosError {message: 'Request failed with status code 404', name: 'AxiosError', code: 'ERR_BAD_REQUEST', config: {…}, request: XMLHttpRequest, …}
-
   render() {
+    const { classes } = this.props;
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <h1>Customer Add</h1>
-        Photo: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
-        Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
-        Birthday: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
-        Gender: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
-        Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
-        <button type="Submit">Add</button>
-      </form>
+      // <form onSubmit={this.handleFormSubmit}>
+      //   <h1>Customer Add</h1>
+      //   Photo: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+      //   Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+      //   Birthday: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
+      //   Gender: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
+      //   Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
+      //   <button type="Submit">Add</button>
+      // </form>
+      <div>
+        <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+          Add Customer
+        </Button>
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+          <DialogTitle>Add Customer</DialogTitle>
+          <DialogContent>
+            <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+            <label htmlFor="raised-button-file">
+              <Button variant="contained" color="primary" component="span" name="file">
+                {this.state.fileName === "" ? "Choose Photo!" : this.state.fileName}
+              </Button>
+            </label>
+            <br/><br/>
+            <TextFileld label="Name" type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+            <TextFileld label="Birthday" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
+            <TextFileld label="Gender" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
+            <TextFileld label="Job" type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>Add</Button>
+            <Button variant="outlined" color="primary" onClick={this.handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     )
   }
 }
 
-export default CustomerAdd;
+// export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,31 @@
 import React from "react";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
 
 class CustomerDelete extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
+  }
 
   deleteCustomer(id) {
     const url = '/api/customers/' + id;
@@ -12,7 +37,24 @@ class CustomerDelete extends React.Component {
 
   render() {
     return (
-      <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>Delete</button>
+      // <button onClick={(e) => {this.deleteCustomer(this.props.id)}}>Delete</button>
+      <div>
+        <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>Delete</Button>
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+          <DialogTitle onClose={this.handleClickOpen}>
+            Delete Alert
+          </DialogTitle>
+          <DialogContent>
+            <Typography gutterBottom>
+              Selected customer profile will be deleted!
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color="primary" onClick={(e) => {this.deleteCustomer(this.props.id)}}>Delete</Button>
+            <Button variant="outlined" color="primary" onClick={this.handleClose}>Close</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     )
   }
 }
